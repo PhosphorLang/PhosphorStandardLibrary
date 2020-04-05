3e486c9a368490d3d28acfade275c472cd5762a3
Benedikt Magnus <magnus@magnuscraft.de>
2020-04-05 17:28:00 +0200

    File descriptors are signed integers

--- src/standard.c
+++ src/standard.c
@@ -9,7 +9,7 @@ extern __attribute__((noreturn)) void exit ();
 
 void print (const void* address, unsigned long long size)
 {
-    unsigned long long file_descriptor = 1; // File descriptor ID for stdout
+    long long file_descriptor = 1; // File descriptor ID for stdout
     unsigned long long syscode = 1; // Syscall ID for writing
 
     asm("syscall" : : "d" (size), "S" (address), "D" (file_descriptor), "a" (syscode));
