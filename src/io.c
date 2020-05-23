b247e1434d53838de80fe1c84687adcf03c1c3c2
Benedikt Magnus <magnus@magnuscraft.de>
2020-05-23 10:22:06 +0200

    Added readLine function.

--- src/io.c
+++ src/io.c
@@ -1,5 +1,6 @@
-#include "types.h"
+#include "memory.h"
 #include "string.h"
+#include "types.h"
 
 /**
  * Writes a string to the standard output, followed by a line break.
@@ -21,4 +22,70 @@ void writeLine (const String text)
 
     // TODO: Check result.
     // TODO: Replace hardcoded line break with a system dependent constant.
+}
+
+/**
+ * Reads a line from the standard input.
+ * @return The string that has been read in. Does not include the line break.
+ */
+String readLine ()
+{
+    Int fileDescriptor = 0; // File descriptor ID for stdin
+    UInt syscode = 0; // Syscall ID for reading
+
+    const UInt bufferSize = 4096;
+    UInt8 buffer[bufferSize];
+
+    Int bytesRead = 0;
+    UInt resultSize = 0;
+    UInt8* result = null;
+
+    while (true)
+    {
+        asm volatile ("syscall" : "=a" (bytesRead) : "d" (bufferSize), "S" (&buffer[0]), "D" (fileDescriptor), "a" (syscode));
+
+        // TODO: Check for errors (bytesRead is -1).
+
+        if (bytesRead > 0)
+        {
+            // Allocate memory for the new total result size:
+            UInt newSize = resultSize + bytesRead;
+            UInt8* newResult = alloc(newSize);
+
+            // Copy the old result and the buffer to the new result:
+            copy(newResult, result, resultSize);
+            copy(&newResult[resultSize], buffer, bytesRead);
+
+            // Don't forget: Free the old result!
+            free(result, resultSize);
+
+            result = newResult;
+            resultSize = newSize;
+
+            // We end reading as soon as the last character is a line break:
+            if (buffer[bytesRead - 1] == '\n') // TODO: Replace the hard coded line break with a system dependent constant.
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    UInt stringSize = resultSize;
+
+    // Remove the line break at the end if there is one:
+    if (result[stringSize - 1] == '\n') // TODO: Replace the hard coded line break with a system dependent constant.
+    {
+        stringSize--;
+    }
+
+    String readString = createString(result, stringSize);
+
+    // Free the full result, including the potentially ignored line break:
+    free(result, resultSize);
+
+    return readString;
 }
