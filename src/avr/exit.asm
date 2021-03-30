db5a18b957e66aaefb99b0ea274238c7d3bc9f3d
Benedikt Magnus <magnus@magnuscraft.de>
2021-03-30 09:00:00 +0200

    AVR: Fixed that the exit function would be interrupted by the watchdog.

--- src/avr/exit.asm
+++ src/avr/exit.asm
@@ -4,3 +4,4 @@
     exit:
         cli ; Disable all interrupts.
         sleep ; Put the microcontroller into sleep mode.
+        rjmp .-4 ; As soon as the watchdog stops the sleep mode, go back to it.
