89d92477d5856a41a4977a4b27e5c23bc4a02e70
Benedikt Magnus <magnus@magnuscraft.de>
2022-10-02 21:21:44 +0200

    Build script adjustments for new target "Backseat"

--- build.sh
+++ build.sh
@@ -140,6 +140,31 @@ function targetAvr
     copyHeaders "$targetName"
 }
 
+function targetBackseat
+{
+    local targetName="backseat"
+    local targetSubdirectory="backseat"
+
+    prepare "$targetName" "$targetSubdirectory"
+
+    # For Backseat, we combine all Bssembler files into a single one which will function as the standard library:
+    local standardLibraryFile="$BINARY_DIRECTORY/$targetName/standardLibrary.bsm"
+    touch "$standardLibraryFile"
+
+    for sourceFile in "$PLATFORM_SOURCE_DIRECTORY/$targetSubdirectory"/*.bsm ; do
+        # Check if this is really a file and otherwise skip the loop:
+        if ! [ -f $sourceFile ]; then
+            continue
+        fi
+
+        cat "$sourceFile" >> "$standardLibraryFile"
+        # New line:
+        echo "" >> "$standardLibraryFile"
+    done
+
+    copyHeaders "$targetName"
+}
+
 # Compile the contents of a subdirectory with the given compile command.
 function compileSubdirectory
 {
@@ -212,10 +237,14 @@ function build
         all)
             targetAvr
             targetAmd64
+            targetBackseat
             targetLinuxAmd64
             ;;
         avr)
             targetAvr
+            ;;
+        backseat)
+            targetBackseat
             ;;
         amd64)
             targetAmd64
@@ -240,12 +269,12 @@ case $target in
     clean)
         clean
         ;;
-    all|avr|amd64|linuxAmd64|stable)
+    all|avr|amd64|backseat|linuxAmd64|stable)
         build $target
         ;;
     -h|--help|help|*)
         echo "Platform targets:"
-        echo "  avr, amd64, limuxAmd64"
+        echo "  avr, amd64, backseat, limuxAmd64"
         echo "Special targets:"
         echo "  clean - Clean the build directory."
         echo "  all - Build all targets."
