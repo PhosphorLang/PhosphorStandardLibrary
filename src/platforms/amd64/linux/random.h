ae087d4fc7c24e81b735ff954eb56bfb9f032a52
Benedikt Magnus <magnus@magnuscraft.de>
2024-03-07 18:40:47 +0100

    Fixed the "Random" module's ASM names in the C header.

--- src/platforms/amd64/linux/random.h
+++ src/platforms/amd64/linux/random.h
@@ -2,6 +2,6 @@
 
 #include "../../common/types.h"
 
-void randomise () asm ("Standard.Random.randomise");
+void randomise () asm ("\"Standard.Random~randomise\"");
 
-Integer getRandom (Integer range) asm ("Standard.Random..getRandom");
+Integer getRandom (Integer range) asm ("\"Standard.Random~getRandom\"");
